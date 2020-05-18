7e4676001756721f9ae76d32b7539827b7e445d3
antpXD <piotrekantoniakk@gmail.com>
2020-05-18 21:40:36 +0200

    delete junk

--- test.js
+++
@@ -1,8 +0,0 @@
-const TorrentSearchApi = require("torrent-search-api");
-
-TorrentSearchApi.enableProvider("ThePirateBay");
-
-// Search '1080' in 'Movies' category and limit to 20 results
-const torrents = TorrentSearchApi.search("the invisible", "All", 2);
-
-torrents.then(data => console.log(data)).catch(error => console.log(error));
